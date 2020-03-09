eeb1ed08623f4d6404267e99143aab8c2255bc4f
sivalingamtce <sivalingamsd@gmail.com>
2020-03-09 12:34:18 +0530

    add attributes and relationship into the person.js

--- app/models/order.js
+++ app/models/order.js
@@ -1,5 +1,6 @@
 import Model from '@ember-data/model';
+import DS from 'ember-data';
 
 export default Model.extend({
-
+    lineItems: DS.hasMany('line-item')
 });
--- app/models/person.js
+++ app/models/person.js
@@ -1,5 +1,9 @@
 import Model from '@ember-data/model';
+import DS from 'ember-data';
 
-export default Model.extend({
-
+export default DS.Model.extend({
+    firstName: DS.attr('string'),
+    lastName: DS.attr('string'),
+    birthday: DS.attr('date')
 });
+
